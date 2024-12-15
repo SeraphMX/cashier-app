9436fca2ef8358b53d7a312512d47cda2f3d7aa5
Edgar Moreira Ortiz <seraph.axl@gmail.com>
2024-12-15 00:06:23 -0600

    Update src/main.tsx [BoltSync]

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { NextUIProvider } from '@nextui-org/react';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <Provider store={store}>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </Provider>
   </StrictMode>
 );
