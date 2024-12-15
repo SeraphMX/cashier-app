f75ba8b1f7b8fa97c17443e5f0e296e562e26e8c
Edgar Moreira Ortiz <seraph.axl@gmail.com>
2024-12-15 00:06:33 -0600

    Update src/components/money/BillCounter.tsx [BoltSync]

---
+++ src/components/money/BillCounter.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, CardBody } from '@nextui-org/react';
+import { DenominationCount } from '../../types/money';
+import { NumberControls } from '../common/NumberControls';
+
+interface BillCounterProps {
+  bill: DenominationCount;
+  onCountChange: (denomination: number, count: number) => void;
+}
+
+export const BillCounter: React.FC<BillCounterProps> = ({ bill, onCountChange }) => {
+  return (
+    <Card>
+      <CardBody>
+        <div className="flex items-center justify-between">
+          <div className="w-32 text-lg font-semibold">
+            ${bill.value.toLocaleString()}
+          </div>
+          <NumberControls
+            value={bill.count}
+            onChange={(value) => onCountChange(bill.value, value)}
+          />
+          <div className="w-32 text-right text-default-500">
+            = ${(bill.value * bill.count).toLocaleString()}
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
