33aa6cfe6339d7c93f51a5258967553a834d4ed8
Edgar Moreira Ortiz <seraph.axl@gmail.com>
2024-12-15 00:06:46 -0600

    Update src/types/money.ts [BoltSync]

--- src/types/money.ts
+++ src/types/money.ts
@@ -7,3 +7,10 @@ export interface MoneyState {
   bills: DenominationCount[];
   coins: number;
 }
+
+export interface CashWithdrawal {
+  id: string;
+  amount: number;
+  details: MoneyState;
+  timestamp: string;
+}
